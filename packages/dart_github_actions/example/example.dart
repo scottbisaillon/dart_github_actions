5489d9f7f9974d275861490df57f5ba3dca02c0c
scottbisaillon <scott.bisaillon.dev@gmail.com>
2022-09-02 16:41:55 -0400

    chore(dart_github_actions): add example

---
+++ packages/dart_github_actions/example/example.dart
@@ -0,0 +1,47 @@
+import 'package:dart_github_actions/dart_github_actions.dart';
+
+void main() async {
+  const basePath = 'packages/dart_github_actions';
+
+  await Workflow(
+    name: 'dart_github_actions',
+    on: [
+      Push(
+        branches: ['master', 'development'],
+        paths: [
+          '.github/workflows/build_dart_github_actions.yaml',
+          '$basePath/**',
+        ],
+      )
+    ],
+    jobs: [
+      Job(
+        id: 'build',
+        runsOn: RunnerType.ubuntuLatest,
+        defaults: const Defaults(
+          run: Run(workingDirectory: 'packages/dart_github_actions'),
+        ),
+      )
+        ..uses(
+          const CustomAction(
+            actionOwner: 'actions',
+            actionName: 'checkout',
+            actionVersion: 'v3',
+            inputs: {},
+          ),
+        )
+        ..uses(
+          const CustomAction(
+            actionOwner: 'dart-lang',
+            actionName: 'setup-dart',
+            actionVersion: 'v1',
+            inputs: {},
+          ),
+        )
+        ..run('dart pub get', name: 'Install Dependencies')
+        ..run('dart format --set-exit-if-changed .', name: 'Format')
+        ..run('dart analyze --fatal-infos --fatal-warnings .', name: 'Analyze')
+        ..run('dart run coverage:test_with_coverage', name: 'Run Tests')
+    ],
+  ).writeYamlToFile();
+}
